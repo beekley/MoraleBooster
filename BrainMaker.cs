038a8f0c0eeae6d3e50195dd6bcd23bd765cc7ac
Brett Beekley <brettbeekley@gmail.com>
2016-05-29 10:08:26 -0700

    Reference Building
    The reference building tool (BrainMaker.cs) is probably correctly
    building the reference object. I'm just having trouble serializing the
    nested object using the .NET JSON serializer. Next step is to try
    JSON.NET

--- BrainMaker.cs
+++ BrainMaker.cs
@@ -1,26 +1,48 @@
-class Node {
-	
-	// 0-8 on the board
-	public int space { get; set }
-	
-	// Tree depth
-	public int depth { get; set }
-	
-	// true if...
-	public bool result { get; set; }
-	
-	// Children nodes
-	public Node[] child { get; set; }
-	
-	// Generate child nodes
-	public void generateChildren() {
-		for ( int i = 0; i < 7; i++ ) {
-			this.child
-		}
-	}
-	
-};
+using System;
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
 
-// Root node
-var tree = new Node { depth = 0 };
-tree.generateChildren();
+namespace HelloWorld
+{
+	public class Node {
+				
+		// Tree depth
+		public int depth;
+		
+		// true if...
+		public bool result;
+		
+		// Children nodes
+		public List<Node> child = new List<Node>();
+		
+		// Generate child nodes
+		public void generateChildren() {
+			for ( int i = 0; i < 8 - this.depth; i++ ) {
+				this.child.Add(new Node{ depth = this.depth + 1 });
+				this.child[i].generateChildren();
+			}
+		}
+		
+	};
+
+    class Hello 
+    {
+        static void Main() 
+        {
+			// Root node
+			var tree = new Node { depth = 0 };
+			tree.generateChildren();
+			Console.WriteLine(tree.child[7].child[6].child[5].child[4].child[3].child[2].child[1].child[0]);
+			Console.Write("Done.");
+			Console.Read();
+			
+
+			var serializer = new JavaScriptSerializer();
+			string json = serializer.Serialize(tree);
+
+			//string json = JsonConvert.SerializeObject(Node);
+
+			System.IO.File.WriteAllText(@"C:\Users\brett.beekley\Documents\GitHub\MoraleBooster\json.txt", json);
+        }
+    }
+}
