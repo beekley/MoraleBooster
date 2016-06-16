a5df966ab6b80dcaaa20d1c6fc3948eb570216bc
Brett Beekley <brettbeekley@gmail.com>
2016-06-16 11:05:21 -0700

    It works!
    P2 can force a win! The JSON now has all games simulated. The root
    actually has a result "-1", but all of its children at "2", which
    indicates that the hypothesis is correct.

--- Program.cs
+++ Program.cs
@@ -17,6 +17,10 @@ namespace brain
 
         // Children nodes
         public List<Node> children = new List<Node>();
+        // Parent node
+        public Node parent;
+        // Is this node the last child?
+        public bool lastChild = false;
 
         // Generate child nodes
         public void generateChildren()
@@ -24,6 +28,14 @@ namespace brain
             for (int i = 0; i < 9 - this.depth; i++)
             {
                 this.children.Add(new Node { depth = this.depth + 1 });
+                this.children[i].parent = this;
+
+                // if last child
+                if (i == 8 - this.depth)
+                {
+                    this.children[i].lastChild = true;
+                }
+
                 this.children[i].generateChildren();
             }
         }
@@ -43,6 +55,48 @@ namespace brain
             }
             // int is non nullable, so return something outside of range
             return -1;
+        }
+
+        // Assigns the result to the node
+        // if this is the last child of parent, then assign result to parent node
+        public void assignResult(int result)
+        {
+            this.result = result;
+            if (this.lastChild)
+            {
+                // 1 if P1 forced win
+                // 2 if else
+                int r = 0;
+                int player = this.parent.depth % 2 + 1;
+                foreach (var child in this.parent.children)
+                {
+                    // if P1 turn, all children must have result = 1
+                    if (player == 1)
+                    {
+                        if (child.result == 1)
+                        {
+                            r = 1;
+                        }
+                        else
+                        {
+                            r = 2;
+                            break;
+                        }
+                    }
+                    // if P2 turn, any child must have result = 1
+                    else
+                    {
+                        r = 2;
+                        if (child.result == 1)
+                        {
+                            r = 1;
+                        }
+                    }
+                }
+
+                // assign result to parent
+                this.parent.assignResult(r);
+            }
         }
 
         // abbreviated for file size
@@ -72,7 +126,7 @@ namespace brain
 
         // initialize current node
         // This is used to keep track of where we are in the tree
-        Node currentNode;
+        public Node currentNode;
         public Game(Node node)
         {
             currentNode = node;
@@ -87,12 +141,19 @@ namespace brain
         // return false
         // else
         // goto 1
-        public bool play()
+        public int play()
         {
             while(inProgress)
             {
                 // find the open space that corresponds to the next move
-                int move = this.findOpenSpace(this.currentNode.findNextMove());
+                int childNumber = this.currentNode.findNextMove();
+                int move = this.findOpenSpace(childNumber);
+
+                // No remaining moves
+                if (move == -1)
+                {
+                    return -1;
+                }
 
                 // P1 if depth is even, P2 if odd
                 int currentPlayer = this.currentNode.depth % 2 + 1;
@@ -103,17 +164,17 @@ namespace brain
                 // Check for end
                 int status = this.checkWin(move);
 
-                switch (status)
+                if(status != 0)
                 {
-                    case 1:
-                        return false;
-                    case 2:
-                        return true;
-                    case 3:
-                        return false;
+                    // delete children that will never be played
+                    this.currentNode.children.Clear();
+                    inProgress = false;
+                    return status;
                 }
+
+                this.currentNode = this.currentNode.children[childNumber];
             }
-            return false;
+            return -1;
         }
 
         // return (n+1)th empty space
@@ -124,12 +185,11 @@ namespace brain
             {
                 if(board[i] == 0)
                 {
-                    counter++;
-                    
                     if (counter == n)
                     {
                         return i;
                     }
+                    counter++;
                 }
             }
             return -1;
@@ -158,7 +218,8 @@ namespace brain
             else if ((board[0] == board[4] &&
                 board[0] == board[8]) ||
                 (board[2] == board[4] &&
-                board[2] == board[6]))
+                board[2] == board[6]) &&
+                board[i] == board[4])
             {
                 return board[4];
             }
@@ -185,10 +246,21 @@ namespace brain
             tree.generateChildren();
             Console.WriteLine(tree.children[8].children[7].children[6].children[5].children[4].children[3].children[2].children[1].children[0]);
 
-            var game = new Game(tree);
-            Console.WriteLine(game.play());
+            int i = 0;
 
-            //System.IO.File.WriteAllText(@"C:\Users\brett.beekley\Documents\GitHub\MoraleBooster\json.txt", tree.serializeJSON());
+            while (true)
+            {
+                var game = new Game(tree);
+                int result = game.play();
+                game.currentNode.assignResult(result);
+                if (result == -1)
+                {
+                    break;
+                }
+                i++;
+            }
+            
+            System.IO.File.WriteAllText(@"C:\Users\brett.beekley\Documents\GitHub\MoraleBooster\json.txt", tree.serializeJSON());
 
             Console.Write("Done.");
             Console.Read();
